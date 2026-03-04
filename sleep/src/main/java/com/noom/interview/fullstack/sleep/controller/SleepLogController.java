d487b65cf73ffddd633b673108ecd239550512e2
Bogdan Bitfoi <bbitfoi@grubhub.com>
2026-03-04 16:28:06 +0200

    Remove unused imports

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/controller/SleepLogController.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/controller/SleepLogController.java
@@ -4,8 +4,6 @@ import com.noom.interview.fullstack.sleep.dto.CreateSleepLogRequest;
 import com.noom.interview.fullstack.sleep.dto.SleepAveragesResponse;
 import com.noom.interview.fullstack.sleep.model.SleepLog;
 import com.noom.interview.fullstack.sleep.service.SleepLogService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
